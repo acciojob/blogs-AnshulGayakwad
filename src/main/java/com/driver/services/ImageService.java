f5bb5595219511337ae77e63b75745bd53d5fa8c
Anshul <anshulgayakwad12345@gmail.com>
2023-11-23 20:59:13 +0530

    6th Final Commit

--- src/main/java/com/driver/services/ImageService.java
+++ src/main/java/com/driver/services/ImageService.java
@@ -46,6 +46,7 @@ public class ImageService {
     public int countImagesInScreen(Integer id, String screenDimensions) {
         //Find the number of images of given dimensions that can fit in a screen having `screenDimensions`
         //Image image = imageRepository2.findById(id).orElse(null);
+        /*
         Image image = imageRepository2.findById(id).get();
         String screenDimension[] = screenDimensions.split("X");
         String imgDimension[] = image.getDimensions().split("X");
@@ -54,5 +55,44 @@ public class ImageService {
         int integerImage = Integer.parseInt(imgDimension[0])*Integer.parseInt(imgDimension[1]);
 
         return integerScreen/integerImage;
+        */
+
+        //4X4 = 4/2*4/2 == 2*2== 4 images
+        Image image = imageRepository2.findById(id).get();
+
+        String imageDimensions = image.getDimensions();
+        //image dimension is in String format ex: 2X2
+        //we have to convert it into integer like 2*2=4
+        //below is the process to calculate image dimension as width and height
+
+        int indexOfX = imageDimensions.indexOf('X');
+
+        String x = imageDimensions.substring(0,indexOfX);
+        String y = imageDimensions.substring(indexOfX+1);
+
+        int imageWidth = Integer.parseInt(x);
+        int imageHeight= Integer.parseInt(y);
+
+
+        //Like above, Similarly find screen dimension in integer format
+
+        int screenIndexOfX = screenDimensions.indexOf('X');
+
+        String screenX = screenDimensions.substring(0,screenIndexOfX);
+        String screenY = screenDimensions.substring(screenIndexOfX+1);
+
+        int screenWidth = Integer.parseInt(screenX);
+        int screenHeight = Integer.parseInt(screenY);
+
+        //THIS COMMENTED LOGIC IS NOT WORKING DON'T KNOW WHY. ASK IN DOUBT
+        // int totalImageDimension = imageWidth * imageHeight;
+        // int totalScreenDimension = screenWidth * screenHeight;
+        // int count = totalScreenDimension/totalImageDimension;
+
+
+        // Final count
+        int count = (screenWidth/imageWidth) * (screenHeight/imageHeight);
+
+        return count;
     }
 }
